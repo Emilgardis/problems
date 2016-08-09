d3bf709e79dc0325f920798be16603e6ba8d6f5f
Emil Gardström <emil.gardstrom@gmail.com>
2016-08-09 16:55:20 +0200

    Added sieve.
    Also added some documentation.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,107 @@
 //! A collatz max length thing.
 //!
+//! # TODO
+//! * Use less clones, currently, the program clones pretty 
+//! much every u64 that is passed around. It is better to use references.
+//! This will make the program much less memory-hungry
+//! * Implement logging.
+//! * Put stuff in Boxes, don't know if this will fix much of the memory usage though.
+//! * Make interactive, probably with a CLI or passing flags.
+//! * Make sieve and the for-loop threaded.
 use std::iter::Iterator;
 use std::collections::BTreeMap;
 // use std::default::Default;
 // extern crate copperline as cl;
 
-
-// Holds all known values that go to 1, except for powers of two. TODO: Add a closure for
-// powers.
-struct CollatzSieve(BTreeMap<u64, u64>, BTreeMap<u64, u64>, bool); // TODO: Make generic over all Sized and Eq
+/// A sieve for our number crunching
+///
+/// # TODO
+/// * Make thread-safe (?), it may actually already be.
+pub struct CollatzSieve {
+    /// Holds all known values that go to 1, except for powers of two.
+    /// The tuple includes `(steps, origin)`.
+    pub sieve: BTreeMap<u64, (u64, u64)>,
+    /// Holds the total steps required to get to 1, together with `sieve`
+    /// this allows us to compute the steps required to get to 1.
+    /// This is register easily by computing `total_steps` **-** `steps`
+    pub sieve_data: BTreeMap<u64, u64>,
+    /// Set to `true` to disable the sieve functionality.
+    pub dummy: bool,
+    /// Unimplemented.
+    ///
+    /// FIXME
+    /// This will be used as a way of seeing how the `BTreeMap`s are called.
+    /// This way we can probably find a better solution.
+    _access_debug: BTreeMap<u64, u64>,
+}// TODO: Make generic over all Sized and Eq
 
 impl CollatzSieve {
-    fn new(dummy: bool) -> CollatzSieve {
-        CollatzSieve(BTreeMap::new(), BTreeMap::new(), dummy)
-    }
-
-    fn insert(&mut self, val: u64, orig: u64) {
-        if !self.2 {
-            self.0.insert(val, orig);
+    /// Make a new sieve. Set dummy to true to disable any actual functionality.
+    pub fn new(dummy: bool) -> CollatzSieve {
+        CollatzSieve {
+            sieve: BTreeMap::new(),
+            sieve_data: BTreeMap::new(),
+            dummy: dummy,
+            _access_debug: BTreeMap::new(),
         }
     }
+    /// Insert data about a certain value. 
+    ///
+    /// # Fields
+    /// * `val` is the number we are handling
+    /// * `steps` is the amount of steps taken from `orig` to get to `val`
+    /// * `orig` is the starting point, used as a kind of has for `sieve_data`
+    pub fn insert(&mut self, val: u64, steps: u64, orig: u64) {
+        if !self.dummy { 
+            self.sieve.insert(val, (steps, orig));
+        }
+    }
+    
+    /// Adds `orig` to `sieve_data`. This means we now have complete knowledge of
+    /// any new number we've gone throuqh.
+    pub fn add_result(&mut self, orig: u64, total_steps: u64) {
+        if !self.dummy { 
+            self.sieve_data.insert(orig, total_steps);
+        }
+    }
+
     /// Checks if this entry exists, if it does, how many steps to tumble down?
-    fn get_previous(&self, val: &u64) -> Option<u64> {
-        match self.0.get(val) {
-            Some(ref orig) => {
-                match self.1.get(orig) {
-                    Some(counted) => Some(counted.clone()),
-                    None => panic!("Expected to find a result."),
-                }
-            },
-            None => None
+    ///
+    /// # Panics
+    /// Panics if we know about a value, but not about it's `orig`. This is probably
+    /// something that hinders thread-safety.
+    pub fn in_sieve(&self, val: &u64) -> Option<u64> {
+        if !self.dummy {
+            match self.sieve.get(val) {
+                Some(&(steps, orig)) => {
+                    match self.sieve_data.get(&orig) {
+                        Some(total_steps) => {
+                            Some(total_steps - steps)
+                        },
+                        None => panic!("Expected to find a result."),
+                    }
+                },
+                None => None
+            }
+        } else {
+            None
         }
     }
 }
 
-struct Collatz<'a> {
-    orig: u64,
-    curr: u64,
+/// Used to iterate over the sequence.
+///
+/// # Fields
+pub struct Collatz<'a> {
+    /// The starting point
+    pub orig: u64,
+    /// The current value we are processing
+    pub curr: u64,
     //walked: Vec<u64>,
-    count: u64,
-    sieve: &'a mut CollatzSieve,
+    /// The amount of steps we have taken to get to `curr`
+    pub count: u64,
+    /// The actual sieve we are using.
+    pub sieve: &'a mut CollatzSieve,
 }
 
 
@@ -47,35 +109,41 @@ struct Collatz<'a> {
 impl<'a> Iterator for Collatz<'a> {
     type Item = u64;
     fn next(&mut self) -> Option<u64> {
-        // println!("{0}\t{1} - {1:#b}", self.orig, self.curr);
+        //if self.orig == 8400511 { println!("{0}\t{1} - {1:#b} n^2: {2:?}", self.count, self.curr, self.curr.is_power_of_two()); }
+        match self.sieve.in_sieve(&self.curr) {
+            Some(steps) => {
+                //println!("{} found {} in sieve, added {} steps", self.orig, self.curr, steps+1);
+                self.count += steps+1;
+                self.register();
+                return None;
+            },
+            None => {},
+        }
         if self.curr == 1 {
+            self.register();
             return None;
-        //} else if self.sieve.0.contains(&self.curr) {
-            // idk how to do this shit. Maybe a map?
-            // FIXME: Add proper count for sieved values.
         } else if self.curr.is_power_of_two() {
-            // FIXME: Speed?
-            // Should not cause any problems.
             self.count += self.curr.trailing_zeros() as u64 - 1;
+            self.register();
             return None;
         } else if self.curr % 2 == 0 {
             self.count += 1;
             //self.walked.push(self.curr);
-            self.sieve.insert(self.curr, self.orig);
+            self.sieve.insert(self.curr, self.count, self.orig);
             self.curr = self.curr / 2;
         } else {
             self.count += 1;
             //self.walked.push(self.curr);
-            self.sieve.insert(self.curr, self.orig);
+            self.sieve.insert(self.curr, self.count, self.orig);
             self.curr = (3 * self.curr)+1; // We know from math that odd*odd + 1 is even. 
-            // But that will rest for now. Add a function to do the first condition.
+            // But that will rest for now.
         }
         Some(self.curr)
     }
 }
 
 impl<'a> Collatz<'a> {
-    fn new(start: u64, sieve: &'a mut CollatzSieve) -> Collatz {
+    pub fn new(start: u64, sieve: &'a mut CollatzSieve) -> Collatz {
         Collatz {
             orig: start,
             curr: start,
@@ -84,12 +152,21 @@ impl<'a> Collatz<'a> {
             sieve: sieve,
         }
     }
+    /// Called before the iterator returns `None`. Sets the appropriate values
+    /// `orig` and `total_steps` to the sieve's `sieve_data`
+    /// 
+    /// # TODO
+    /// Make this more seamless, currently calling `register` before returning `None`
+    /// is registere only because I'm not sure how to implement this. Possibly with `Drop`?
+    pub fn register(&mut self) {
+        self.sieve.add_result(self.orig, self.count);
+    }
 }
 
 fn main() {
     let mut sieve = CollatzSieve::new(true);
     let mut max = (1, 1); // Longest chain, length.
-    for i in 1..(1000000 as u64) {
+    for i in 1..(1e6 as u64 + 1) {
         let mut coll = Collatz::new(i, &mut sieve);
         while let Some(_) = coll.next() {
             //print!("{},", num);
@@ -101,5 +178,6 @@ fn main() {
         }
     }
 
-    println!("Longest chain was ({}, {}). \nLength of sieve is {}", max.0, max.1, sieve.0.len())
+    println!("Longest chain was ({}, {}). \nLength of sieve is {}", max.0, max.1, sieve.sieve.len());
+    //println!(":::\n{:?}\n:::\n{:?}", sieve.0, sieve.1);
 }
